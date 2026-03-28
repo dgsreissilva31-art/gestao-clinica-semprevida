d80544ee18db2dee4a38ae6b9686fdf169f2c9f9
dgsreissilva31-art <dgsreissilva31@gmail.com>
2026-03-28 13:51:51 -0300

    Update settings.py to include middleware and SSL header
    Added middleware for security and Whitenoise support.

--- settings.py
+++ settings.py
@@ -28,3 +28,21 @@ DATABASES = {
 ROOT_URLCONF = 'urls'
 WSGI_APPLICATION = 'wsgi.application'
 STATIC_URL = 'static/'
+
+
+
+
+
+
+
+import os
+
+# Procure a lista de MIDDLEWARE e adicione esta linha no topo dela:
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # Adicione esta se quiser carregar imagens/estilos
+    # ... as outras que já estão lá ...
+]
+
+# No final do arquivo, adicione isso para a Railway não te bloquear:
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
