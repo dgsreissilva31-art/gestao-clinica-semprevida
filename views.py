bbad82268612f4fa8af0d40dd9b808d08497b2e0
dgsreissilva31-art <dgsreissilva31@gmail.com>
2026-04-11 13:08:29 -0300

    Update views.py

--- views.py
+++ views.py
@@ -7,7 +7,6 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from django.contrib.auth.models import User
-from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 
 
 
