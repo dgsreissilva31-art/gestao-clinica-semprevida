4508d3c0a65a3fb1b96902262422c70c8a632392
dgsreissilva31-art <dgsreissilva31@gmail.com>
2026-04-11 14:05:14 -0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -3402,6 +3402,8 @@ urlpatterns = [
     path('prontuario/', prontuario_geral),
     path('caixa/', caixa_geral),
     path('admin-painel/', painel_controle, name='painel'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
    
 
    
