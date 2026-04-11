97c4bb1d592c6f095f47e2929808e9868ace5b48
dgsreissilva31-art <dgsreissilva31@gmail.com>
2026-04-11 14:01:17 -0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -5,7 +5,7 @@ from django.db import connection
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
- 
+import views 
  
 
 
