5a6c8db096e3473169c72bf09285c090007c47f4
dgsreissilva31-art <dgsreissilva31@gmail.com>
2026-04-14 23:42:11 -0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -807,21 +807,23 @@ def exames_geral(request):
                 ex = cursor.fetchone()
                 nome_exame = ex[0] if ex else "Exame"
 
-                cursor.execute("""
-                    INSERT INTO caixa
-                    (paciente_nome, profissional_nome, valor, forma_pagamento, status, categoria, descricao, data_pagamento, unidade_id)
-                    VALUES (%s,%s,%s,%s,%s,%s,%s,CURRENT_DATE,%s)
-                """, [
-                    paciente,
-                    prestador,
-                    valor,
-                    forma,
-                    'Pago',
-                    'Exame',
-                    nome_exame,
-                    unidade_id
-                ])
+               usuario_responsavel = request.user.username  # ✅ captura quem está logado
 
+cursor.execute("""  
+    INSERT INTO caixa  
+    (paciente_nome, profissional_nome, valor, forma_pagamento, status, categoria, descricao, data_pagamento, unidade_id, usuario_lancamento)  
+    VALUES (%s,%s,%s,%s,%s,%s,%s,CURRENT_DATE,%s,%s)  
+""", [  
+    paciente,  
+    prestador,  
+    valor,  
+    forma,  
+    'Pago',  
+    'Exame',  
+    nome_exame,  
+    unidade_id,  
+    usuario_responsavel  # ✅ novo campo
+])
             mensagem = '<div class="alert alert-success">✅ Exame lançado no caixa!</div>'
 
         except Exception as e:
